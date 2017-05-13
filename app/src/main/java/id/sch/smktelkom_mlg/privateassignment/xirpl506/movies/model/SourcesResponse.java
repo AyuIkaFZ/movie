df8836a420e92307eb4c6728dd20a6263258b81d
AyuIkaFZ <ayuika116@gmail.com>
2017-05-13 19:30:21 +0700

    fix semua fragment

---
+++ app/src/main/java/id/sch/smktelkom_mlg/privateassignment/xirpl506/movies/model/SourcesResponse.java
@@ -0,0 +1,13 @@
+package id.sch.smktelkom_mlg.privateassignment.xirpl506.movies.model;
+
+import java.util.List;
+
+/**
+ * Created by hyuam on 18/04/2017.
+ */
+
+public class SourcesResponse {
+    public String status;
+    public List<Result> results;
+
+}
